ebb36f380275991c63455ca0d3635d6fb3419bea
Jorma Rebane <jorma.rebane@gmail.com>
2022-10-14 15:34:16 +0300

    mamabuild: added mamafile.py

---
+++ mamafile.py
@@ -0,0 +1,21 @@
+import mama
+
+##
+# Explore Mama docs at https://github.com/RedFox20/Mama
+#
+class googletest(mama.BuildTarget):
+
+    def dependencies(self):
+        pass
+
+    def configure(self):
+        pass
+
+    def package(self):
+        self.export_includes(['include'], build_dir=True)
+        # export gmock and gtest main libs only 
+        self.export_libs('lib/', [
+            'gmock.lib', 'gmock.a',
+            'gtest.lib', 'gtest.a'
+        ])
+
